fb85ae61fe68aa4861979c2e4bbc247b8223ca9f
tempe-techie <95053628+tempe-techie@users.noreply.github.com>
2023-10-10 20:28:53 +0000

    Deploying to build from @ BasePunkOrg/basepunk-chat@10c6753cd97978ac77a3335ce9ccf4a729cf1477 🚀

---
+++ _nuxt/tokens.bc8ceea4.js
@@ -0,0 +1,1 @@
+const s=[{chainId:8453,address:"0x0000000000000000000000000000000000000000",decimals:18,name:"ETH",symbol:"ETH",swap:!0},{chainId:8453,address:"0x4200000000000000000000000000000000000006",decimals:18,name:"Wrapped ETH",symbol:"WETH",swap:!0}],e={tokens:s};export{e as t};
